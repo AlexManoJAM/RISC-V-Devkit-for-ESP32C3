58c562e9ba68136c3ffbf139dc3116f874d23db1
AlexManoJAM <35406517+AlexManoJAM@users.noreply.github.com>
2022-08-10 13:46:10 +0100

    Update batch_1.bat

--- SDK_Setup/msys2_dependencies/batch_1.bat
+++ SDK_Setup/msys2_dependencies/batch_1.bat
@@ -1,2 +1,2 @@
-bash -l -c "pacman -Syu && pacman -S --needed base-devel mingw-w64-x86_64-toolchain && pacman -S mingw-w64-x86_64-cmake"
+bash -l -c "pacman -Syu && pacman -S --needed base-devel mingw-w64-x86_64-toolchain && pacman -S mingw-w64-x86_64-cmake && pacman -S make"
 

4497e3c9114a93af4a6503e769aa42ae26176b01
AlexManoJAM <35406517+AlexManoJAM@users.noreply.github.com>
2022-08-11 16:31:08 +0100

    Update setup.bat

--- SDK_Setup/setup.bat
+++ SDK_Setup/setup.bat
@@ -1,12 +1,12 @@
 rem Bypass "Terminate Batch Job" prompt.
-rem if "%~1"=="-FIXED_CTRL_C" (
-rem  REM Remove the -FIXED_CTRL_C parameter
-rem   SHIFT
-rem ) ELSE (
-   REM Run the batch with <NUL and -FIXED_CTRL_C
-rem   CALL <NUL %0 -FIXED_CTRL_C %*
-rem   GOTO :EOF
-rem)
+  if "%~1"=="-FIXED_CTRL_C" (
+ REM Remove the -FIXED_CTRL_C parameter
+   SHIFT
+ ) ELSE (
+  REM Run the batch with <NUL and -FIXED_CTRL_C
+   CALL <NUL %0 -FIXED_CTRL_C %*
+   GOTO :EOF
+)
 
 
  IF EXIST msys2_dependencies\msys2_path.txt ( echo msys2 already installed
@@ -15,11 +15,11 @@ rem)
          START /WAIT msys2-x86_64-20220603.exe
       ) else (
        powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/msys2/msys2-installer/releases/download/2022-06-03/msys2-x86_64-20220603.exe' -OutFile msys2-x86_64-20220603.exe  }" 
+       pause
        START /WAIT msys2-x86_64-20220603.exe 
        Del msys2-x86_64-20220603.exe
        )
-
- )
+   )
  
 
  IF EXIST xpack-riscv-none-embed-gcc-10.2.0-1.2 (
@@ -28,46 +28,37 @@ rem)
      powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/xpack-dev-tools/riscv-none-embed-gcc-xpack/releases/download/v10.2.0-1.2/xpack-riscv-none-embed-gcc-10.2.0-1.2-win32-x64.zip' -OutFile xpack-riscv-none-embed-gcc-10.2.0-1.2-win32-x64.zip }"  
      tar -xf xpack-riscv-none-embed-gcc-10.2.0-1.2-win32-x64.zip
     )
-::IF EXIST mdk-main (
-::    echo Framework already extracted
-::    ) else (
-::    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/cpq/mdk/archive/refs/heads/main.zip' -OutFile main.zip }" 
-::    tar -xf main.zip
-::    )
-
-::IF EXIST openocd-esp32c3-modifications-main (
- ::   echo Openocd executable already extracted
- ::   ) else (
-::    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/AlexManoJAM/openocd-esp32c3-modifications/archive/refs/heads/main.zip' -OutFile openocd-esp32c3-modifications-main.zip }" 
- ::    tar -xf openocd-esp32c3-modifications-main.zip
- ::    tar -xf openocd-esp32c3-modifications-main\openocd-esp32.zip    
-  ::  )
 
  IF EXIST xpack-riscv-none-embed-gcc-10.2.0-1.2-win32-x64.zip (
     echo deleting toolchain zip
     Del xpack-riscv-none-embed-gcc-10.2.0-1.2-win32-x64.zip
     )
 
-Set "_tempvar="
-If EXIST RISC-V-Devkit-for-ESP32C3-SDK Set "_tempvar=1"
-If EXIST esp32c3-SDK-main Set "_tempvar=1"
-if %_tempvar% EQU 1 (
+Set tempvar=
+
+   If EXIST RISC-V-Devkit-for-ESP32C3-SDK Set tempvar=1
+   If EXIST esp32c3-SDK-main Set tempvar=1
+
+   IF %tempvar% EQU 1 (
+   
     echo Framework already extracted
-    ren RISC-V-Devkit-for-ESP32C3-SDK esp32c3-SDK-main
     ) else (
         powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/AlexManoJAM/RISC-V-Devkit-for-ESP32C3/archive/refs/heads/SDK.zip' -OutFile main.zip }" 
         tar -xf main.zip
         ren RISC-V-Devkit-for-ESP32C3-SDK esp32c3-SDK-main
     )
 
+
   IF EXIST openocd-esp32 ( 
     echo Openocd already extracted
+    
     ) else (
-        powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/AlexManoJAM/RISC-V-Devkit-for-ESP32C3/archive/refs/heads/OpenOCD.zip' -OutFile open.zip }" 
-        tar -xf open.zip
-        tar -xf RISC-V-Devkit-for-ESP32C3-OpenOCD/openocd-esp32.zip
-        Del RISC-V-Devkit-for-ESP32C3-OpenOCD
+      powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/AlexManoJAM/RISC-V-Devkit-for-ESP32C3/archive/refs/heads/OpenOCD.zip' -OutFile open.zip }" 
+      tar -xf open.zip
+      tar -xf RISC-V-Devkit-for-ESP32C3-OpenOCD/openocd-esp32.zip
+      rmdir /s /Q RISC-V-Devkit-for-ESP32C3-OpenOCD
     )
+
     
    IF EXIST  main.zip (
       echo deleting Framework zip
@@ -82,6 +73,7 @@ if %_tempvar% EQU 1 (
 
    IF EXIST RISC-V-Devkit-for-ESP32C3-OpenOCD (
       echo deleting Openocd folder
+
       rmdir /s /Q RISC-V-Devkit-for-ESP32C3-OpenOCD
       )
    
@@ -89,18 +81,18 @@ if %_tempvar% EQU 1 (
       echo deleting msys2-x86_64-20220603.exe
       Del msys2-x86_64-20220603.exe
       )
-   
-   if EXIST RISC-V-Devkit-for-ESP32C3-SDK(
+   IF EXIST RISC-V-Devkit-for-ESP32C3-SDK (
       echo deleting RISC-V-DEVKIT-for-ESP32C3-SDK
+      echo before rmdir
       rmdir /s /Q RISC-V-DevKit-for-ESP32C3-SDK
       )
-   
 
+set curdir= %cd%
 cd /
-for /F "delims=" %%I in ('dir /B /S bash.exe | findstr "msys64"') do set DIRPATH=%%I
-rem set DIRPATH=%DIRPATH:bash.exe=%
+for /F "delims=" %%I in ('dir /B /S bash.exe ^| findstr "msys64"') do set DIRPATH=%%I
+set DIRPATH=%DIRPATH:bash.exe=%
+cd %curdir%
+echo %DIRPATH% > msys2_dependencies\msys2_path.txt
 echo %DIRPATH% > esp32c3-SDK-main\mdk-main\tools\msys2_path.txt
 Set PATH=%DIRPATH%
-echo 1 >instalation_verification.txt
-pause
 START /WAIT cmd /K msys2_dependencies\batch_1

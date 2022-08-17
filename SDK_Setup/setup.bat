b6195f020bc0636c90b2a9b41a06b805a4e25efb
AlexManoJAM <35406517+AlexManoJAM@users.noreply.github.com>
2022-08-17 16:40:19 +0100

    Update setup.bat

--- SDK_Setup/setup.bat
+++ SDK_Setup/setup.bat
@@ -1,21 +1,22 @@
 rem Bypass "Terminate Batch Job" prompt.
-  if "%~1"=="-FIXED_CTRL_C" (
+REM  if "%~1"=="-FIXED_CTRL_C" (
  REM Remove the -FIXED_CTRL_C parameter
-   SHIFT
- ) ELSE (
+REM    SHIFT
+REM ) ELSE (
   REM Run the batch with <NUL and -FIXED_CTRL_C
-   CALL <NUL %0 -FIXED_CTRL_C %*
-   GOTO :EOF
-)
+ REM  CALL <NUL %0 -FIXED_CTRL_C %*
+ REM   GOTO :EOF
+ REM)
+set msy=0
 
-
- IF EXIST msys2_dependencies\msys2_path.txt ( echo msys2 already installed
+  IF EXIST msys2_dependencies\msys2_path.txt ( 
+   echo msys2 already installed
+   set msy=1
    ) else (
       IF EXIST msys2-x86_64-20220603.exe (
          START /WAIT msys2-x86_64-20220603.exe
       ) else (
        powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/msys2/msys2-installer/releases/download/2022-06-03/msys2-x86_64-20220603.exe' -OutFile msys2-x86_64-20220603.exe  }" 
-       pause
        START /WAIT msys2-x86_64-20220603.exe 
        Del msys2-x86_64-20220603.exe
        )
@@ -33,14 +34,13 @@ rem Bypass "Terminate Batch Job" prompt.
     echo deleting toolchain zip
     Del xpack-riscv-none-embed-gcc-10.2.0-1.2-win32-x64.zip
     )
-
-Set tempvar=
-
+    
+   Set tempvar=0
    If EXIST RISC-V-Devkit-for-ESP32C3-SDK Set tempvar=1
-   If EXIST esp32c3-SDK-main Set tempvar=1
-
-   IF %tempvar% EQU 1 (
    
+   If EXIST esp32c3-SDK-main Set tempvar=1
+   
+   IF %tempvar% EQU 1 (
     echo Framework already extracted
     ) else (
         powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/AlexManoJAM/RISC-V-Devkit-for-ESP32C3/archive/refs/heads/SDK.zip' -OutFile main.zip }" 
@@ -48,18 +48,15 @@ Set tempvar=
         ren RISC-V-Devkit-for-ESP32C3-SDK esp32c3-SDK-main
     )
 
-
   IF EXIST openocd-esp32 ( 
     echo Openocd already extracted
-    
     ) else (
       powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://github.com/AlexManoJAM/RISC-V-Devkit-for-ESP32C3/archive/refs/heads/OpenOCD.zip' -OutFile open.zip }" 
       tar -xf open.zip
       tar -xf RISC-V-Devkit-for-ESP32C3-OpenOCD/openocd-esp32.zip
       rmdir /s /Q RISC-V-Devkit-for-ESP32C3-OpenOCD
     )
-
-    
+  
    IF EXIST  main.zip (
       echo deleting Framework zip
       Del main.zip
@@ -73,7 +70,6 @@ Set tempvar=
 
    IF EXIST RISC-V-Devkit-for-ESP32C3-OpenOCD (
       echo deleting Openocd folder
-
       rmdir /s /Q RISC-V-Devkit-for-ESP32C3-OpenOCD
       )
    
@@ -87,12 +83,27 @@ Set tempvar=
       rmdir /s /Q RISC-V-DevKit-for-ESP32C3-SDK
       )
 
-set curdir= %cd%
-cd /
-for /F "delims=" %%I in ('dir /B /S bash.exe ^| findstr "msys64"') do set DIRPATH=%%I
-set DIRPATH=%DIRPATH:bash.exe=%
-cd %curdir%
-echo %DIRPATH% > msys2_dependencies\msys2_path.txt
-echo %DIRPATH% > esp32c3-SDK-main\mdk-main\tools\msys2_path.txt
-Set PATH=%DIRPATH%
-START /WAIT cmd /K msys2_dependencies\batch_1
+
+IF %msy% EQU 0 ( GOTO:SET_DIRPATH
+ ) else (
+   GOTO:READ_DIRPATH
+ )
+:READ_DIRPATH
+   set /p DIRPATH=< msys2_dependencies\msys2_path.txt
+   Set PATH=%DIRPATH% 
+   GOTO:END
+
+:SET_DIRPATH:
+   set curdir= %cd%
+   cd /
+   for /F "delims=" %%I in ('dir /B /S bash.exe ^| findstr "msys64"') do set DIRPATH=%%I  
+   set DIRPATH=%DIRPATH:bash.exe=%
+   cd %curdir%
+   echo %DIRPATH% > msys2_dependencies\msys2_path.txt
+   echo %DIRPATH% > esp32c3-SDK-main\mdk-main\tools\msys2_path.txt
+   Set PATH=%DIRPATH%
+
+:END
+   START /WAIT cmd /K msys2_dependencies\batch_1
+ 
+ 
